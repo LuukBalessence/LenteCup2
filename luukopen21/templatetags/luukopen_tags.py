2433bbb98535a5b20d44d3f4ebe29242cecf3155
luuks <developer@balessence.nl>
2021-09-09 23:34:00 +0200

    Home menu aangepast

--- luukopen21/templatetags/luukopen_tags.py
+++ luukopen21/templatetags/luukopen_tags.py
@@ -1,3 +1,5 @@
+import decimal
+
 from django import template
 
 register = template.Library()
@@ -5,7 +7,7 @@ register = template.Library()
 
 @register.simple_tag
 def playing_handicap(hcp, sr, cr, par):
-    division_factor = 2.0 if par < 40.0 else 1.0
+    division_factor = decimal.Decimal(2.0) if par < 40.0 else decimal.Decimal(1.0)
     phcp = int(round((hcp*(sr/113)+(cr-par))/division_factor))
     return phcp
     
