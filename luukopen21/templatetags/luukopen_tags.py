c3a8c56a6f802cf1897a0173d2ea9050b4ecdf94
unknown <jfcsmeets@gmail.com>
2021-09-09 16:19:55 +0200

    Corrected for 9 holes

--- luukopen21/templatetags/luukopen_tags.py
+++ luukopen21/templatetags/luukopen_tags.py
@@ -2,8 +2,10 @@ from django import template
 
 register = template.Library()
 
+
 @register.simple_tag
 def playing_handicap(hcp, sr, cr, par):
-    phcp = int(round(hcp*(sr/113)+(cr-par)))
+    division_factor = 2.0 if par < 40.0 else 1.0
+    phcp = int(round((hcp*(sr/113)+(cr-par))/division_factor))
     return phcp
     
