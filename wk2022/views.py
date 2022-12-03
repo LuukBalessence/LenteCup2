6a219cf5f3198e9ffabb4a87e1c8d29bd536b134
luuks <luuksmeets@hotmial.com>
2022-12-03 16:58:57 +0100

    kleine wijziging wk2022

--- wk2022/views.py
+++ wk2022/views.py
@@ -2094,3 +2094,59 @@ def koscheme(request):
     komatches = VirtualMatch.objects.filter(koreference__icontains="F", home__in=allleagueteams)
     return render(request, "wk2022/koscheme.html",
                   context={"error": error, "komatches": komatches})
+
+
+# Usage: python extract_match_data.py <URL>
+# Example:
+# python extract_match_data.py "https://sporza.be/nl/sport/voetbal/~3311844/"
+import sys
+import urllib.request
+from urllib.request import urlopen
+import json
+
+
+def extract_match_data(url):
+    # Open url and read data into string
+    data = urlopen(url).read()
+    body = data.decode('utf-8')
+    # Split string into list of lines
+    body_lines = body.split('\n')
+    # Search for the line containing the page data, and check that there is just one line. Use list comprehension
+    info_line = [item for item in body_lines if ('data-hypernova-key="Scoreboard"' in item and 'reqUrl' in item)]
+    assert len(info_line) == 1
+    # Convert 1-element list to string
+    info_line = info_line[0]
+    # Poor man's way of searching for start and end statement of json
+    index_string_start = info_line.find('<!--')
+    index_string_end = info_line.find('-->')
+    # Parse json
+    json_string = info_line[index_string_start + 4: index_string_end]
+    match_data = json.loads(json_string)
+    # Return the dictionary
+    return match_data
+
+
+def updatescores:
+    url = sys.argv[1]
+    match_data = extract_match_data(url)
+    print('Home squad')
+    print('----------')
+    for home_player in match_data['data']['homeSquad']['lineup']:
+        print(home_player['player']['name'])
+    print('')
+    print('Away squad')
+    print('----------')
+    for away_player in match_data['data']['awaySquad']['lineup']:
+        print(away_player['player']['name'])
+    # Events are ordered last to first, reverse them in display
+    for event in match_data['data']['events'][::-1]:
+        if 'GOAL' in event['type']:
+            print('{} minute: {} {}: {}-{} ({} points)'.format(
+                event['time'], event['player']['firstName'], event['player']['lastName'], event['homeScore'],
+                event['awayScore'],
+                1.0 if event['type'] == 'GOAL' else 0.5 if event['type'] == 'PENALTY_GOAL' else 0.0))
+
+
+if __name__ == '__main__':
+    main()
+
